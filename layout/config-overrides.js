59d8d37642d6ba32ee54123102f74cf8c1060196
davindev <vinnie.jeong@kidsnote.com>
2023-12-14 14:38:52 +0900

    chore: react-app-rewired 패키지 설치 및 webpack.config override 처리

---
+++ layout/config-overrides.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  webpack: (config) => {
+    return {
+      ...config,
+      entry: './src/index',
+      mode: 'development',
+      output: {
+        path: path.resolve(__dirname, 'build'),
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+        }),
+        new ModuleFederationPlugin({
+          name: 'layout',
+          filename: 'remoteEntry.js',
+          exposes: {
+            './Header': './src/components/Header',
+          }
+        }),
+      ],
+      module: {
+        rules: [{
+          test: /\.(ts|tsx)$/i,
+          loader: 'ts-loader',
+          exclude: ['/node_modules/'],
+        }],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+      },
+    }
+  },
+  devServer: (configFunction) => {
+    return (proxy, allowedHost) => ({
+      ...configFunction(proxy, allowedHost),
+      open: true,
+      port: 3000,
+      host: 'localhost',
+      static: {
+        directory: path.resolve(__dirname, 'build'),
+      },
+    });
+  },
+};
