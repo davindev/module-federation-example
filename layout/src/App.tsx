d5dd19b7a7653e47b5f4ac328859cff1e454ba10
davindev <vinnie.jeong@kidsnote.com>
2023-12-15 09:36:01 +0900

    feat: 공통 Header 컴포넌트 UI 수정

--- layout/src/App.tsx
+++ layout/src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { ChakraProvider, Flex } from '@chakra-ui/react';
+
+import Header from './components/Header';
 
 export default function App() {
-  return <div>Layout</div>;
+  return (
+    <ChakraProvider>
+      <Flex
+        flexDirection="column"
+        justifyContent="space-between"
+        height="100vh"
+      >
+        <Header />
+      </Flex>
+    </ChakraProvider>
+  );
 }
--- layout/src/components/Header.tsx
+++ layout/src/components/Header.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import { BellIcon, SettingsIcon } from '@chakra-ui/icons';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      width="100%"
+      height="60px"
+      paddingX="20px"
+      backgroundColor="#ffffff"
+      borderBottom="1px solid #e9ecef"
+    >
+      <Heading
+        as="h1"
+        fontSize="22px"
+        color="#208df9"
+      >
+        Kidsnote
+      </Heading>
+
+      <Flex columnGap="18px">
+        <Text
+          fontWeight="900"
+          color="#208df9"
+        >
+          키즈노트
+        </Text>
+        <Text
+          fontWeight="900"
+          color="#8c95a1"
+        >
+          전자문서+
+        </Text>
+      </Flex>
+      
+      <Flex
+        columnGap="18px"
+        alignItems="center"
+      >
+        <BellIcon
+          boxSize={6}
+          color="#8c95a1"
+        />
+        <SettingsIcon
+          boxSize={5}
+          color="#8c95a1"
+        />
+      </Flex>
+    </Flex>
+  );
 }
